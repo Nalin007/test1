30e414953f6813b5e7e1b80a5be46f6959789ab6
Nalin007 <52191889+Nalin007@users.noreply.github.com>
2019-06-25 16:08:27 +0700

    Lab01

---
+++ Lab01/Lab01/FastProgram.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+	//cout << "Hello sec C" << endl;
+	//cout << "We will graduate in 4 years" << endl;
+	//cout << "\"Information Teachnology \'C\'\" ?" << endl;
+	//cout << setw(205) << "This is Turbo C++ Program."<<endl;
+	//cout << setw(200) <<"It is very easy." <<endl;
+	//cout << setw(197) <<"I love C++" <<endl; 
+	//cout << setw(205) <<"It\'s high level language." <<endl;
+	//cout << "I want to Programer." << std::endl;
+	//cout << "A = \101 =\x41 "<< endl;
+	//cout << "A = "<<(char)65<< endl; 
+	//cout << "my nickname = "<<(char)76<<(char)101<<(char)107<< endl;
+	//cout << "my name = "<<(char)78<<(char)97<<(char)108<<(char)105<<(char)110<< endl;
+	//unsigned short x = 65535;
+	//cout << "short\t=" << sizeof(x) << " bytes." << x << endl;
+	//cout << "Hexa constant 0x23 is "<< 0x23 << " deximal" << endl;
+	float f=10; 
+	int i=10; 
+	char c='L'; 
+	double d=2250;
+	cout << "f value is " << f << endl;
+	cout << "i value is " << i << endl;
+	cout << "c value is " << c << endl;
+	cout << "d value is " << d << endl;
+
+
+	return 0;
+}
